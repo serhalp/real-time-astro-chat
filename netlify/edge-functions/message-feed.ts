2151c6ad05ca3b86feafe2b8a300a3ba313f66d3
Philippe Serhal <philippe.serhal@netlify.com>
2024-07-18 11:13:31 -0400

    feat: add Server-Sent Events boilerplate

---
+++ netlify/edge-functions/message-feed.ts
@@ -0,0 +1,28 @@
+export default async function handler(): Promise<Response> {
+  let timerId: number | undefined;
+  const body = new ReadableStream({
+    // TODO actually fetch new messages
+    start(controller) {
+      timerId = setInterval(() => {
+        const msg = new TextEncoder().encode(
+          `data: hello at ${new Date().toUTCString()}\r\n\r\n`,
+        );
+        controller.enqueue(msg);
+      }, 1000);
+    },
+    cancel() {
+      if (typeof timerId === "number") {
+        clearInterval(timerId);
+      }
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "Content-Type": "text/event-stream",
+    },
+  });
+}
+
+export const config = {
+  path: "/message-feed",
+};
